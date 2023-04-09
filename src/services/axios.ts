a531dcdf3e39774326693f4aedd01d05aee3480f
Rafael Schettini <rafael.schettini@compasso.com.br>
2023-04-09 17:20:40 -0300

    feat: disable button when loading

--- src/services/axios.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'https://fastify-crud-deploy-api.onrender.com'
-});
--- src/utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date: string) : string => {
-  const formatedDate = new Date(date.replace('Z', ''));
-
-  return formatedDate.toLocaleDateString('en-US', {
-    month: 'short', day: 'numeric', year: 'numeric'
-  });
-};
